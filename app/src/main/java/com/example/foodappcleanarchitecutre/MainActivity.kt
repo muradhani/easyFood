aa0334e4000109fb20d53e62bcf371d3cdda015d
moura <muradhani>
2023-11-16 10:09:01 +0200

    add dagger setup and create android hilt application class and add intuit dependency

--- app/src/main/java/com/example/foodappcleanarchitecutre/MainActivity.kt
+++ app/src/main/java/com/example/foodappcleanarchitecutre/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.foodappcleanarchitecutre
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.foodappcleanarchitecutre.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
